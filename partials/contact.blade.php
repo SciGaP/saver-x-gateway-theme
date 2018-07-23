58967d533a08fd4e7d28d2498681219cb2a41d71
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-23 11:55:34 -0400

    Fixed the title of the page

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-      <h2 class="text-center">About</h2>
+      <h2 class="text-center">Contact Us</h2>
       <hr class="separation-line blue-bg"/>
 
       <p style="font-size:18px; margin-top:20px;" align="left">
