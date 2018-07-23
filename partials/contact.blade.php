a4079ed6e6e6fcae84cedda7b1e96ec025b4930d
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-23 11:53:56 -0400

    Adding content to the Contact page

--- partials/contact.blade.php
+++ partials/contact.blade.php
@@ -1,7 +1,19 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-      <h2 class="title top-space">Contact page</h2>            
+      <h2 class="text-center">About</h2>
+      <hr class="separation-line blue-bg"/>
+
+      <p style="font-size:18px; margin-top:20px;" align="left">
+      Department of Biological Sciences <br>
+      407 Choppin Hall<br>
+      Louisiana State University<br>
+      Baton Rouge, LA 70803<br>
+      Tel: (225) 578-2791<br>
+      Fax: (225) 578-2597<br>
+      www.brylinski.org    <br>
+      </p>
+
     </div>
   </div>  
 </div>
