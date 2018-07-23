c4725177127c6e75868cea8ffc2dc7245fc695ac
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-23 11:57:11 -0400

    Adding title to Documentation page

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -1,1 +1,10 @@
+<div class="container">
+  <div class="content">
+    <div class="text-center">
 
+  <h2 class="text-center">Training and Documentation</h2>
+    <hr class="separation-line blue-bg"/>
+
+    </div>
+  </div>
+</div>
