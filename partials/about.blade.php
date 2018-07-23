4490911c6f98507fb6e05cf5a51eeb21d437dbbe
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2018-07-23 11:43:02 -0400

    Added content to Above page

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -1,7 +1,17 @@
 <div class="container">
   <div class="content">
     <div class="text-center">
-      <h2 class="title top-space">About page</h2>            
+      <h2 class="text-center">About</h2>
+      <hr class="separation-line blue-bg"/>
+
+      <p style="font-size:18px; margin-top:20px;" align="left">
+      The major focus of our group is the design and development of novel tools for the structure-based modeling and analysis of biological networks.
+      Computational Systems Biology can be considered as a complex platform that integrates many algorithms from different research areas such as Structural Bioinformatics, Functional Genomics, Cheminformatics and Pharmacogenomics.
+      We develop computational tools to study the evolution and organization of pathways into biological networks with the primary application in modern drug discovery and design.
+      Biological pathways, which are the common units of biological networks, can be broadly defined as the series of interactions between molecular entities such as proteins, nucleic acids and small organic molecules that trigger a variety of cellular responses.
+      Their malfunction can be often directly linked to many disease states. Our ambitious goal is to reveal the underlying principles of biological network evolution, organization and dynamics.
+      By doing so, we hope to be able to predict the phenotypic outcome of biological network perturbations.
+      </p>
     </div>
   </div>  
 </div>
