6db6a4b2eba8cf574746eab59bb978c11d8b372a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-01-24 12:50:06 -0500

    Adding 'Cite Us'

---
+++ partials/citeus.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+  <div class="content">
+    <div class="text-center">
+      <h2 class="text-center">Cite Us</h2>
+      <hr class="separation-line blue-bg"/>
+
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        It is critical to have our work cited so we can continue to develop exciting tools for you.
+        Please be sure to not only cite https://sciencegateway.brylinski.org, but to also cite the tools that we have used to build this amazing resource.
+        </p>
+        <ul style="list-style-type:square">
+        <li style="font-size:18px" align="left">Abeysinghe, E., Brylinski, M., Christie, M., Marru, S., & Pierce, M. (2018, July). The CSBG-LSU Gateway: Web based Hosted Gateway for Computational System Biology Application Tools from Louisiana State University.</li>
+
+        </ul>
+        <br></br>
+        <h4 class="text-left">Acknowledgements</h4>
+        <p style="font-size:18px; margin-top:20px;" align="left">
+        Development of the Apache Airavata used to develop the science gateway is supported by NSF award #1339774.
+        </p>
+    </div>
+  </div>
+</div>
