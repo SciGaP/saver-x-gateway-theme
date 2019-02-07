d4c22433e33b38d1132d6e356ea9742f495e048e
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-02-07 03:00:06 -0500

    Adding documentation information in to the gateway

--- partials/documentation.blade.php
+++ partials/documentation.blade.php
@@ -2,91 +2,10 @@
   <h2 class="text-center">Training and Documentation</h2>
     <hr class="separation-line blue-bg"/>
 
-    <h4>CSBG (Computational System Biology Group) Science Gateway User Manual</h4>
-      <h5>Gateway Access</h5>
+    <h4>Single Cell RNA Sequencing Application, Saver-X Documentation</h4>
+    <p style="font-size:18px; margin-top:20px;" align="left">
+     Please refer <a href="https://singlecell.wharton.upenn.edu/saver-x/ ">Saver-X Documentation</a> for more information.
 
-        <ul style="list-style-type:square">
-          <li>Create a gateway account in https://sciencegateway.brylinski.org/. Use 'Create account'. Provide your details and you will receive an email for account verification.</li>
-          <li>Click the link in the email received and confirm your email. Hint: The email sometimes tend to land on 'Junk email' or 'Trash'.</li>
-          <li>When gateway administrator grants access, you would receive a notification email.</li>
-          <li>Login to the gateway, You are ready to Search through the SRA!</li>
-        </ul>
-        <img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/csbg-lsu-login.png" alt="Gateway Login Page" style="width:500px;height:300px;" class="img-border">
-    <hr/>
-      <h5>Getting Started</h5>
-          <ul style="list-style-type:square">
-            <li>When logged in you would be in your home page, User Dashboard.</li>
-            <li>Two placeholders, 'Browse Projects' and 'Browse Experiments'</li>
-            <li>As a gateway user, you could browse your old projects or experiments using 'Browse Projects' or 'Browse Experiments'.</li>
-            <li>To create new projects or experiments navigate through the menu bar on top.</li>
-            <li>What is a Project? - Project is a collection of experiments, it groups experiments. When creating an experiment, you can select which project to create with.</li>
-            <li>What is an Experiment? - Experiment is the gateway record you create in order to launch a job in a remote cluster/supercomputer/HPC.</li>
-          </ul>
-          <img src="{{ URL::to('/') }}/themes/{{Session::get('theme')}}/assets/img/csbg-user-dashboard.png" alt="Gateway Dashboard" style="width:500px;height:300px;" class="img-border">
-    <hr/>
-       <h5>Save & Launch an Experiment</h5>
-        <ul style="list-style-type:square">
-          <li>To create a new Experiment from the top menu, navigate to Experiment &rarr; Create</li>
-          <li>Provide;</li>
-            <ul style="list-style-type:square">
-                <li>Experiment Name (Mandatory)</li>
-                <li>Experiment Description (Optional)</li>
-                <li>Project (LOV) - Latest will be the default choice.</li>
-                <li>Application (LOV)</li>
-            </ul>
-          <li>Continue</li>
-                    <li>Provide;</li>
-              <ul style="list-style-type:square">
-                  <li>Sharing Settings (Optional) - Share with fellow gateway users.</li>
-                  <li>Application Inputs (Mandatory)</li>
-              </ul>
-          <li>Save & Launch</li>
-          <br/>
-          <li>When providing Application inputs; </li>
-              <ul style="list-style-type:square">
-                  <li>Target structure (Mandatory)</li>
-                  <li>Provide the rest of the parameters </li>
-                        <ul style="list-style-type:square">
-                               <li>Screening library: BindingDB</li>
-                               <li>Screening library: ChEMBL</li>
-                               <li>Screening library: DrugBank</li>
-                               <li>Screening library: KEGG Compound</li>
-                               <li>Screening library: KEGG Drug</li>
-                               <li>Screening library: NCI-Open</li>
-                               <li>Screening library: RCSB PDB</li>
-                               <li>Screening library: ZINC12</li>
-                  <li>Auxiliary ligands (Optional Input Files. User can provide none and many as pleased)</li>
-              </ul>
-          <li>After providing the files you could 'Save' the experiment for later submission. If saved you could edit the experiment prior to launching.</li>
-          <li>Once launched, you could cancel the experiment, which will cancel the job running in remote cluster. </li>
-        </ul>
-        <p></p>
-    <hr/>
-    <h5>Other Gateway Features</h5>
-    <ul style="list-style-type:square">
-      <li>Users can share their experiments with other gateway users. When sharing, can decide on level of access to grant.</li>
-         <ul style="list-style-type:square">
-             <li>Read Access</li>
-             <li>Write Access</li>
-             </ul>
-       <li>Sharing experiment can be done at any time, at the time of creating, while processing and after the experiment is completed.</li>
-       <li>To create new experiments similar to an existing one, you can clone the experiment from ;Experiment Summary' page.</li>
-    <hr/>
-    <h5>Creating & Browsing Projects</h5>
-    <ul style="list-style-type:square">
-      <li>To create a new Project from the top menu, navigate to Project &rarr; Create</li>
-      <li>Provide;</li>
-        <ul style="list-style-type:square">
-            <li>Project Name (Mandatory)</li>
-            <li>Project Description (Optional)</li>
-            <li>Sharing Settings (Optional)</li>
-        </ul>
-      <li>Save</li>
-      <br/>
-      <li>To browse existing projects, from top menu, navigate Projects &rarr; Browse </li>
-      <li>All your previous projects will be listed. To view the experiments within a project, select a project and click 'View'.</li>
-      <li>You are now in 'Project Summary' page. You can view all the experiments created under the project. Click on experiment name to view individual experiments. </li>
-    </ul>
     <p></p>
 
 </div>
