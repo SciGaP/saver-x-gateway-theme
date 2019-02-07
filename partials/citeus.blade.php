4a1a4bdf18273074b2992db5ce7be77aeacfc55a
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-02-07 03:07:32 -0500

    Added Saver-X citing details

--- partials/citeus.blade.php
+++ partials/citeus.blade.php
@@ -6,12 +6,8 @@
 
         <p style="font-size:18px; margin-top:20px;" align="left">
         It is critical to have our work cited so we can continue to develop exciting tools for you.
-        Please be sure to not only cite https://sciencegateway.brylinski.org, but to also cite the tools that we have used to build this amazing resource.
+        Please be sure to not only cite https://sciencegateway.brylinski.org, but to also cite <a href="https://www.biorxiv.org/content/10.1101/457879v2">Our Publication</a>
         </p>
-        <ul style="list-style-type:square">
-        <li style="font-size:18px" align="left">Abeysinghe, E., Brylinski, M., Christie, M., Marru, S., & Pierce, M. (2018, July). The CSBG-LSU Gateway: Web based Hosted Gateway for Computational System Biology Application Tools from Louisiana State University.</li>
-
-        </ul>
         <br></br>
         <h4 class="text-left">Acknowledgements</h4>
         <p style="font-size:18px; margin-top:20px;" align="left">
