701ce1107198193bbcc7b58fb5916215a7b9cc22
Eroma Abeysinghe <eroma.abeysinghe@gmail.com>
2019-02-07 03:04:09 -0500

    Added about us page

--- partials/about.blade.php
+++ partials/about.blade.php
@@ -1,25 +1,10 @@
 <div class="container">
-  <div class="content">
-    <div class="text-center">
-      <h2 class="text-center">About</h2>
-      <hr class="separation-line blue-bg"/>
+  <h2 class="text-center">Training and Documentation</h2>
+    <hr class="separation-line blue-bg"/>
 
-      <p style="font-size:18px; margin-top:20px;" align="left">
-      The major focus of our group is the design and development of novel tools for the structure-based modeling and analysis of biological networks.
-      Computational Systems Biology can be considered as a complex platform that integrates many algorithms from different research areas such as Structural Bioinformatics, Functional Genomics, Cheminformatics and Pharmacogenomics.
-      We develop computational tools to study the evolution and organization of pathways into biological networks with the primary application in modern drug discovery and design.
-      Biological pathways, which are the common units of biological networks, can be broadly defined as the series of interactions between molecular entities such as proteins, nucleic acids and small organic molecules that trigger a variety of cellular responses.
-      Their malfunction can be often directly linked to many disease states.
-      </p>
+    <p style="font-size:18px; margin-top:20px;" align="left">
+    The Laboratory for Single Cell Data Science at the University of Pennsylvania is home to a multidisciplinary team including statisticians, bioinformaticians and students from a wide range of disciplines such as applied math, medicine and computational biology. We conduct state-of the art research, specifically focussing on methods development for genomic data, analysis of high-throughput experiments, and designing computational models for transcriptomics studies. Recently, our team has diverted its attention to the exciting field of single-cell genomics, wherein we are developing new analytical tools as well as leading hypothesis-driven projects for new biological discoveries.
+    Also refer Saver-X <a href="https://singlecell.wharton.upenn.edu">Parent Site</a>
+    <p></p>
 
-      <p style="font-size:18px; margin-top:20px;" align="left">
-      Our ambitious goal is to reveal the underlying principles of biological network evolution, organization and dynamics.
-      By doing so, we hope to be able to predict the phenotypic outcome of biological network perturbations.
-      </p>
-
-      <p style="font-size:18px; margin-top:20px;" align="left">
-      Currently we offer web services to model protein structures and infer their molecular functions. More tools will become available in the near future.
-      </p>
-    </div>
-  </div>  
 </div>
